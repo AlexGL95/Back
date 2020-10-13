36725da6a93e2df99b0c938f88004100a745a586
Abl <asdasd>
2020-10-13 15:41:33 -0500

    guardarRC v1 completado

--- src/Reporte_ciudadano/dto/reporte-ciudadano.dto.ts
+++ src/Reporte_ciudadano/dto/reporte-ciudadano.dto.ts
@@ -1,10 +1,30 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
 export class ReporteCiudadanoDTO {
+    @IsString()
+    @IsOptional()
     nombre?: string;
+
+    @IsString()
+    @IsOptional()
     telefono?: string;
+
+    @IsString()
+    @IsOptional()
     correo?: string;
+
+    @IsString()
     cp: string;
+
+    @IsString()
     colonia: string;
+
+    @IsNumber()
     categoria: number;
+
+    @IsNumber()
     area: number;
+
+    @IsNumber()
     reporte: string;
 }
