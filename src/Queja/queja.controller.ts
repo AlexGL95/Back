b64bc2e8a58acc16cb25a5325e77e3ab29daa02a
Alex <glopezalex631@gmail.com>
2020-10-13 15:29:46 -0500

    Quejasv1

--- src/Queja/queja.controller.ts
+++ src/Queja/queja.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { createquejadto } from './Dto/queja.dto';
+import { QuejaService } from './queja.service';
 
 @Controller('queja')
-export class QuejaController {}
+export class QuejaController {
+
+    constructor(
+        private quejaservice: QuejaService
+    ){}
+
+    @Post()
+    create(@Res() res, @Body() createqueja: createquejadto){
+        this.quejaservice.createqueja(createqueja).then( queja => {
+            res.status(HttpStatus.CREATED).json(queja)
+        }).catch(()=>{
+            res.status(HttpStatus.CONFLICT).json({mensaje:'Error en la creacion de la queja'});
+        });
+    }
+}
