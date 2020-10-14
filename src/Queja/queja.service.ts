408594418e26b32250f5ec4acf785b22e3b51101
Daniel Alejandro Marin Magaña <daniel.marin.electronica25@gmail.com>
2020-10-14 15:57:18 -0500

    algo

--- src/Queja/queja.service.ts
+++ src/Queja/queja.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Queja } from 'src/Queja/queja.entity';
-var moment = require('moment');
 import { Moment } from "moment";
 import { Repository } from 'typeorm';
 import { createquejadto } from './Dto/queja.dto';
