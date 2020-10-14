692ce1420e9ff63ab1d240ad2811fa45c79edde3
Alex <glopezalex631@gmail.com>
2020-10-14 15:59:00 -0500

    Loginv1

--- src/Auth/auth.controller.ts
+++ src/Auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res, UnauthorizedException, HttpStatus } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsuarioService } from '../Usuario/usuario.service';
+import { authcredentialsdto } from './Dto/authcredentials.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(
+        private auth: UsuarioService,
+        private jwtService: JwtService,
+    ){}
+
+    @Post('signin')
+    async signIn(@Body()authcredentials:authcredentialsdto, @Res() response){
+        let user = await this.auth.validatepass(authcredentials);
+        if (!user) {
+            throw new UnauthorizedException('Credenciales Invalidas');
+        } else {
+            let correo= user.correo;
+            const payload = { correo };
+            const accesstoken = await this.jwtService.sign(payload);
+            let res = {token: accesstoken}
+            response.status(HttpStatus.ACCEPTED).json(res);
+        }
+    }
+}
--- src/Auth/auth.module.ts
+++ src/Auth/auth.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { Usuario } from '../Usuario/usuario.entity';
+import { UsuarioModule } from '../Usuario/usuario.module';
+import { JwtStrategy } from './jwt.strategy';
+import { UsuarioService } from '../Usuario/usuario.service';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Usuario]),
+  PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+            secret:'Cadenasuperindecifrable',
+            signOptions:{
+                expiresIn: 3600,
+            },
+        }),
+        JwtModule.register({
+          secret:'Cadenasuperindecifrable',
+          signOptions:{
+              expiresIn: 3600,
+          },
+      }),
+        UsuarioModule
+],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [
+    AuthService,
+    JwtStrategy,
+    UsuarioService
+  ],
+  exports:[
+    PassportModule,
+    JwtStrategy,
+  ]
 })
 export class AuthModule {}
