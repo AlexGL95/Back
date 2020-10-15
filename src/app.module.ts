d941eeccde73e409129b0a6483027584c7f58a9b
Daniel Alejandro Marin Magaña <daniel.marin.electronica25@gmail.com>
2020-10-15 14:24:15 -0500

    Jalar cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { ArchivosService } from './archivos/archivos.service';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: '',
+      password: 'Konjikinogashbell25()',
       database: 'escuchav1',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
